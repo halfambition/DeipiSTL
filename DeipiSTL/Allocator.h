b61c0ba6fce8a739661a3418b2e36d2f755a8d57
deipi <2603252269@qq.com>
2023-07-27 20:14:08 +0800

    新建分配器头文件

---
+++ DeipiSTL/Allocator.h
@@ -0,0 +1,9 @@
+#pragma once
+//allocator必须包含的接口
+//allocator::value_type
+//allocator::pointer
+//allocator::const_pointer
+//allocator::reference
+//allocator::const_reference
+//allocator::size_type
+//allocator::difference_type
