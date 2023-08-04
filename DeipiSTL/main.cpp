b7f9efb3476c1608bf087679f649f1254e20ba0c
deipi <2603252269@qq.com>
2023-08-04 14:02:57 +0800

    recreate

---
+++ DeipiSTL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DeipiSTL
+//
+//  Created by deipi on 2023/8/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
